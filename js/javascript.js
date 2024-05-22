8622c1a9433f4dfe4289d7f3f03af78b7b20a0b5
sebaamasha <127793243+sebaamasha@users.noreply.github.com>
2024-05-22 23:54:57 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,25 @@
 
+       
+function insertToCategoryMenu(data) {
+    const filterButtonsContainer = document.getElementById("filter-buttons");
+    for (const key in data.genre) {
+    const button = document.createElement("button");
+      const category = data.genre[key];
+      button.textContent = category;
+      const sHtml = `<a href='index.php?genre=${category}'>${category}</a>`;
+      button.innerHTML = sHtml;
+      filterButtonsContainer.appendChild(button);
+    }
+    document.getElementById("filter-buttons").appendChild(filterButtonsContainer);
+  }
+  
+  fetch("data/genre.json")
+    .then(response => response.json())
+    .then(data => insertToCategoryMenu(data))
+    .catch(error => {
+      console.error("Error fetching JSON data:", error);
+    });
+
+
+
+    
